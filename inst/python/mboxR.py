96076ba6367d2267315d568e99c21a785a1fcb08
JooYoung Seo <sjysky@gmail.com>
2018-11-04 22:44:56 -0500

    Modifying Python source

--- inst/python/mboxR.py
+++ inst/python/mboxR.py
@@ -1,13 +1,6 @@
 import pandas as pd
-#import sys
 import mailbox
-#import csv
 from email.header import decode_header
-
-#infile = sys.argv[1]
-#outfile = sys.argv[2]
-#infile = "2008-December.mbox"
-#outfile = "out.csv"
 
 def get_content(part):
     content = ''
@@ -18,14 +11,6 @@ def get_content(part):
         for part in payload:
             content += get_content(part)
     return content
-
-#with open(outfile, "w", newline="") as f:
-#    writer = csv.writer(f)
-#    writer.writerow(['date', 'from', 'subject', 'content'])
-#    for index, message in enumerate(mailbox.mbox(infile)):
-#        content = get_content(message)
-#        row = [message['date'], message['from'].strip('>').split('<')[-1], decode_header(message['subject'])[0][0], content]
-#        writer.writerow(row)
 
 def mbox_df(infile):
     row = []
