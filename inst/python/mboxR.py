1fa0b6121f4073ed1ee32c8b006c1b16b31b81ac
JooYoung Seo <sjysky@gmail.com>
2019-07-04 06:17:50 -0400

    Non-ASCII decoding in the subject line has been resolved.

--- inst/python/mboxR.py
+++ inst/python/mboxR.py
@@ -2,7 +2,7 @@
 
 """mboxR
 
-Created by JooYoung Seo, (jooyoung@psu.edu), on Nov 18, 2018.
+Created by JooYoung Seo, (jooyoung@psu.edu), on Nov 18, 2018 (latest updated on July 4, 2019).
 All source codes in this file can be freely modified, repurposed, and shared with this claimer.
 
 The source codes below are tailored for Python 3.x and can be passed into R environment using reticulate package."""
@@ -10,7 +10,7 @@ The source codes below are tailored for Python 3.x and can be passed into R envi
 
 from pandas import DataFrame
 from mailbox import mbox
-from email.header import decode_header
+from email.header import decode_header, make_header
 from email.utils import parsedate_to_datetime
 
 def get_content(part):
@@ -28,7 +28,7 @@ def mbox_df(infile):
         row = []
         for message in mbox(infile):
             content = get_content(message)
-            line = [str(parsedate_to_datetime(message['date'])), message['Message-ID'], message['In-Reply-To'], message['References'], message['from'], message['to'], message['Cc'], decode_header(message['subject'])[0][0], content]
+            line = [str(parsedate_to_datetime(message['date'])), message['Message-ID'], message['In-Reply-To'], message['References'], message['from'], message['to'], message['Cc'], str(make_header(decode_header(message['subject']))), content]
             row.append(line)
     except Exception as e:
         print("Something wrong with your mbox file: ", infile, ". Check the file(s) if it is formed correctly. Maybe 'From ' in the mail body has not been correctly escaped as '>From '. Refer to the following resource:\nhttps://pymotw.com/3/mailbox/\nThe specific error message from Python: ", e, sep="")
